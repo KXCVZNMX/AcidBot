0e42c1bb15299932b94fec6f856892bda1dc7609
KXCVZNMX <CC6391@scotchmel.vic.edu.au>
2025-10-26 00:28:54 +1100

    chores: added small section for home page instruction. TODO is setting up bookmarklet and to use the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,42 +2,41 @@
 
 import { useState } from 'react';
 import Image from 'next/image';
-import AcidP from '../../public/acid.png';
-import { Menu, X } from 'lucide-react';
+import AcidP from '../../public/home/acid.png';
+import loginP from '../../public/home/mdxn_login.png'
+import landingP from '../../public/home/mdxn_landing.png';
+import { Menu } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Home() {
     const [open, setOpen] = useState(true);
 
     const sections = [
-        { name: 'Introduction', href: '#introduction' },
         {
             name: 'Setup',
             href: '#setup',
             sub: [
-                { name: 'Install', href: '#install' },
-                { name: 'Configuration', href: '#config' },
+                { name: 'Login', href: '#login' },
+                { name: 'Setup Bookmarklet', href: '#bookmarklet' },
             ],
         },
         { name: 'How to Use', href: '#how-to-use' },
-        { name: 'Examples', href: '#examples' },
-        { name: 'FAQ', href: '#faq' },
-        { name: 'Credits', href: '#credits' },
     ];
 
     return (
         <>
             <div className={'flex h-screen overflow-hidden'}>
                 <div
-                    className={`flex flex-col bg-gray-900 text-white transition-all duration-300 ${
+                    className={`group relative flex flex-col bg-gray-900 text-white transition-all duration-400 ${
                         open ? 'w-64' : 'w-16'
                     }`}
+                    onMouseEnter={() => setOpen(true)}
+                    onMouseLeave={() => setOpen(false)}
                 >
                     <button
-                        onClick={() => setOpen(!open)}
-                        className={'p-4 hover:bg-gray-800 focus:outline-none'}
+                        className={'p-4 focus:outline-none'}
                     >
-                        {open ? <X /> : <Menu />}
+                        <Menu />
                     </button>
 
                     <nav
@@ -81,12 +80,42 @@ export default function Home() {
                     </nav>
                 </div>
 
-                <div
-                    className={
-                        'flex-1 flex items-center justify-center opacity-70'
-                    }
-                >
-                    <Image src={AcidP} alt={'acid'} height={900} width={900} />
+                <div className={'flex-1 overflow-y-auto scroll-smooth'}>
+                    <div
+                        className={
+                            'min-h-screen flex-1 flex items-center justify-center opacity-70'
+                        }
+                    >
+                        <Image src={AcidP} alt={'acid'} height={900} width={900} />
+                    </div>
+
+                    <section id={'login'} className={'p-10 border-b border-gray-800'}>
+                        <h1 className={'text-3xl font-bold mb-4'}>
+                            Login
+                        </h1>
+                        <div className={'text-md'}>
+                            <p className={'pb-3'}>
+                                Log into your <a href={'https://maimaidx-eng.com/'} className={'text-blue-300 underline'}>maimai DX Net</a> normally
+                            </p>
+                            <div className={'flex flex-row gap-5'}>
+                                <Image src={loginP} alt={'maimai dx net login page'} height={450} width={400} />
+                                <Image src={landingP} alt={'maimai dx net landing page'} height={450} width={240} />
+                            </div>
+
+                        </div>
+                    </section>
+
+                    <section id={'bookmarklet'} className={'p-10 border-b border-gray-800'}>
+                        <h1 className={'text-3xl font-bold mb-4'}>
+                            Setup Bookmarklet
+                        </h1>
+                    </section>
+
+                    <section id={'how-to-use'} className={'p-10 border-b border-gray-800'}>
+                        <h1 className={'text-3xl font-bold mb-4'}>
+                            How to Use
+                        </h1>
+                    </section>
                 </div>
             </div>
         </>
