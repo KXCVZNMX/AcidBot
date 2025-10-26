47d0829beaffca7a2968aad485da447672235f57
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2025-10-26 23:29:05 +1100

    chores: changed <section>-s structures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,30 +90,33 @@ export default function Home() {
                         <Image src={AcidP} alt={'acid'} height={900} width={900} />
                     </div>
 
-                    <section id={'login'} className={'p-10 border-b border-gray-800'}>
+                    <section id={'setup'} className={'p-10 border-b border-gray-800'}>
                         <h1 className={'text-3xl font-bold mb-4'}>
-                            Login
+                            Setup
                         </h1>
-                        <div className={'text-md'}>
-                            <p className={'pb-3'}>
-                                Log into your <a href={'https://maimaidx-eng.com/'} className={'text-blue-300 underline'}>maimai DX Net</a> normally
-                            </p>
-                            <div className={'flex flex-row gap-5'}>
-                                <Image src={loginP} alt={'maimai dx net login page'} height={450} width={400} />
-                                <Image src={landingP} alt={'maimai dx net landing page'} height={450} width={240} />
+                        <section id={'login'} className={'p-10 pt-2 border-b border-gray-800'}>
+                            <h1 className={'text-3xl font-bold mb-4'}>
+                                Login
+                            </h1>
+                            <div className={'text-md'}>
+                                <p className={'pb-3'}>
+                                    Log into your <a href={'https://maimaidx-eng.com/'} className={'text-blue-300 underline'}>maimai DX Net</a> normally
+                                </p>
+                                <div className={'flex flex-row gap-5'}>
+                                    <Image src={loginP} alt={'maimai dx net login page'} height={450} width={400} />
+                                    <Image src={landingP} alt={'maimai dx net landing page'} height={450} width={240} />
+                                </div>
+
                             </div>
-
-                        </div>
+                        </section>
+                        <section id={'bookmarklet'} className={'p-10 border-b border-gray-800'}>
+                            <h1 className={'text-2xl font-bold mb-4'}>
+                                Setup Bookmarklet
+                            </h1>
+                        </section>
                     </section>
-
-                    <section id={'bookmarklet'} className={'p-10 border-b border-gray-800'}>
-                        <h1 className={'text-3xl font-bold mb-4'}>
-                            Setup Bookmarklet
-                        </h1>
-                    </section>
-
                     <section id={'how-to-use'} className={'p-10 border-b border-gray-800'}>
-                        <h1 className={'text-3xl font-bold mb-4'}>
+                        <h1 className={'text-2xl font-bold mb-4'}>
                             How to Use
                         </h1>
                     </section>
