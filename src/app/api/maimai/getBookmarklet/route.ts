6423e0c195ea9e51516bc041d488710fd5e71509
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2025-12-15 22:13:06 +0800

    restart: project restart

--- src/app/api/maimai/getBookmarklet/route.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'node:fs';
-import { NextResponse } from 'next/server';
-import { bookmarklet } from '@/lib/util/bookmarklet.js';
-
-export async function GET() {
-    return NextResponse.json({ bookmarklet: bookmarklet });
-}
