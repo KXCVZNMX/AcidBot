ae98d8a7b6bf542eb02f22aace56d85e5caa230d
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2025-12-19 10:58:02 +0800

    chores(remove): `RANK_RULES` is no longer needed

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -38,20 +38,3 @@ export const COMBO_RULES: [string, string][] = [
     ["music_icon_fcp", "FC+"],
     ["music_icon_fc",  "FC"],
 ];
-
-export const RANK_RULES: [string, string][] = [
-    ["music_icon_sssp", "SSS+"],
-    ["music_icon_sss",  "SSS"],
-    ["music_icon_ssp",  "SS+"],
-    ["music_icon_ss",   "SS"],
-    ["music_icon_s+",   "S+"],
-    ["music_icon_s",    "S"],
-    ["music_icon_aaa",  "AAA"],
-    ["music_icon_aa",   "AA"],
-    ["music_icon_a",    "A"],
-    ["music_icon_bbb",  "BBB"],
-    ["music_icon_bb",   "BB"],
-    ["music_icon_b",    "B"],
-    ["music_icon_c",    "C"],
-    ["music_icon_d",    "D"],
-];
