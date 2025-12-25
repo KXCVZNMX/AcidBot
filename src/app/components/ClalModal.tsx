a8bb3f1befaa1b05513a653cbb12ef9bb8f0feaa
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2025-12-25 22:32:50 +0800

    fix: made useEffect only render once

--- src/app/components/ClalModal.tsx
+++ src/app/components/ClalModal.tsx
@@ -14,7 +14,7 @@ export default function ClalModal(
         if (clalCookie) {
             setClal(clalCookie);
         }
-    })
+    }, [])
 
     const setUserClal = async () => {
         try {
