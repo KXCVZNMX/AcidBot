2d7908daad1c72e78c1cad2438822bb81621708b
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2025-12-27 00:02:42 +0800

    feat: finished the guide page with the bookmarklet api

--- src/app/api/getClal/route.ts
+++ src/app/api/getClal/route.ts
@@ -1,10 +1,27 @@
-import {readFile} from "node:fs/promises";
-import path from "node:path";
 import {NextResponse} from "next/server";
 
 export async function GET() {
-    const filePath = path.join(process.cwd(), 'src/lib/getClalCode.js')
-    const code = await readFile(filePath, 'utf8');
+    const site_link = process.env.SITE_LINK ?? 'https://acid.kvznmx.com';
+
+    const code = "function reject() {\n" +
+        "    return prompt(\"Do not change the url after copying it from the site\");\n" +
+        "}\n" +
+        "\n" +
+        "javascript: void(function(d) {\n" +
+        "    if (d.location.host === 'lng-tgk-aime-gw.am-all.net') {\n" +
+        "        let code = new URL(document.location.href).searchParams.get('id') ?? reject();\n" +
+        "        if (code) {\n" +
+        "            let cookie = d.cookie.match(/clal=([^;]*)/)?.[1];\n" +
+        "            if (cookie.length !== 64) return alert(\"Error!\\nI can\\'t fetch your session, try logging in again or switch browsers.\");\n" +
+        `            let win = window.open(\`${site_link}/api/setUserClal?id=$\{code}&clal=$\{cookie}\`, '_blank');
+` +
+        "        } else if (code === null) {\n" +
+        "            console.log(\"User cancelled\");\n" +
+        "        } else {\n" +
+        "            alert(\"Wrong code! Please do not change the url after copying it from the site\");\n" +
+        "        }\n" +
+        "    } else return alert(\"Error!\\nYou are not in the right website\");\n" +
+        "})(document)"
 
     return new NextResponse(code, {
         headers: {
