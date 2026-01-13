57e3706aeb9433b2d4ca9fcf541c758e35cefab8
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-01-13 18:07:50 +0800

    fix: guide name

--- src/app/pages/Guides/ClalGuide/page.tsx
+++ src/app/pages/Guides/ClalGuide/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 import ClalGuide from '@/app/pages/Guides/ClalGuide/ClalGuide';
 
 export const metadata: Metadata = {
-    title: 'AcidBot | Guides',
+    title: 'AcidBot | Clal Guide',
 };
 
 export default function Page() {
