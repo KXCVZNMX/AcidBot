c4af6856916e876188571e4f1a57c3c0c434dd3e
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-01-14 15:21:14 +0800

    chores: temp UserSettings path

---
+++ src/app/pages/UserSettings/page.tsx
@@ -0,0 +1,10 @@
+import UserSettings from "@/app/pages/UserSettings/UserSettings";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'AcidBot | Settings'
+}
+
+export default function Page() {
+    return <UserSettings />
+}
