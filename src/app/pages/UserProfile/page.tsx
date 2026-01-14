4e9769aae177e928b8cf57d318a1d2575d06d00d
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-01-14 16:17:36 +0800

    feat: added small profile page (very WIP), made it SSR for now

--- src/app/pages/UserProfile/page.tsx
+++ src/app/pages/UserProfile/page.tsx
@@ -1,10 +1,41 @@
-import UserProfile from '@/app/pages/UserProfile/UserProfile';
-import { Metadata } from 'next';
+import Image from "next/image";
+import DefaultUserIcon from '../../../../public/225-default-avatar.svg'
+import {auth} from "@/auth";
 
-export const metadata: Metadata = {
-    title: 'AcidBot | Profile',
-};
+export default async function UserProfile() {
+    const session = await auth();
 
-export default function Page() {
-    return <UserProfile />;
+    return (
+        <div className={'flex items-center justify-center'}>
+            <div className={'p-3 w-full max-w-[1200px] grid grid-cols-2 gap-3'}>
+                <div className={'col-span-2'}>
+                    <div className={'card bg-base-200'}>
+                        <div className={'card-body'}>
+                            <div className={'flex flex-row gap-5'}>
+                                <Image
+                                    src={session!.user?.image ?? DefaultUserIcon}
+                                    alt={'user icon'}
+                                    width={150}
+                                    height={150}
+                                    className={'rounded-full'}
+                                />
+                                <div className={'flex flex-col'}>
+                                    <div className={'card-title pl-6'}>
+                                        <h2 className={'text-3xl font-bold'}>
+                                            {session!.user?.name}
+                                        </h2>
+                                    </div>
+                                    <div className={'card-body'}>
+                                        <h3 className={'text-xl'}>
+                                            <p>Created at: {session!.user?.createdAt.toString().split('T')[0]}</p>
+                                        </h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
