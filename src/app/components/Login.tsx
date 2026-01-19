4a741b9cb05e2e267dbc047d572e82a42b354f8c
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-01-19 18:33:35 +0800

    style: login modal

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import { loginGithub, loginGoogle } from '@/app/components/loginHelper';
+import GoogleIcon from '../../../public/google-icon-logo-svgrepo.svg';
+import GithubIcon from '../../../public/GitHub_Invertocat_Black.svg';
+import Image from "next/image";
 
 export function LoginGithub() {
     return (
         <form action={loginGithub}>
-            <button type={'submit'} className={'btn w-full'}>
-                Login with Github
+            <button type={'submit'} className={'bg-base-300 btn w-full hover:invert transition-[filter] duration-300 border-[#dddddd] border-[1.5]'}>
+                <Image src={GithubIcon} alt={'google icon'} width={25} height={25} className={'inline'}/>
+                <p className={'pl-1.5'}>Login with Github</p>
             </button>
         </form>
     );
@@ -15,7 +19,8 @@ export function LoginGithub() {
 export function LoginGoogle() {
     return (
         <form action={loginGoogle}>
-            <button type={'submit'} className={'btn w-full'}>
+            <button type={'submit'} className={'bg-base-300 btn w-full hover:invert transition-[filter] duration-300 border-[#dddddd] border-[1.5]'}>
+                <Image src={GoogleIcon} alt={'google icon'} width={25} height={25} className={'inline'}/>
                 Login with Google
             </button>
         </form>
