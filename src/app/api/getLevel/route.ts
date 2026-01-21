3c8adf6811cece1d0a49886094835292584d3378
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-01-21 13:33:45 +0800

    fix: wrong error message

--- src/app/api/getLevel/route.ts
+++ src/app/api/getLevel/route.ts
@@ -33,7 +33,7 @@ export async function POST(req: NextRequest) {
         if (results.length === 0) {
             return NextResponse.json(
                 {
-                    error: `Page likely didn't return a redirect, get clal again.`,
+                    error: `Either the page didn't return a redirect (get a new clal), or you don't have any results for this level`,
                 },
                 { status: 500 }
             );
