3d8e87d5da0f5388162032e508637a6831751049
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-03-04 11:20:57 +1100

    feat(B50CardGrid): display song index in B50CardGrid component

--- src/app/components/B50CardGrid.tsx
+++ src/app/components/B50CardGrid.tsx
@@ -39,6 +39,7 @@ export default function B50CardGrid({
                         {newSong.map((song, i) => (
                             <div key={i} className="relative">
                                 <div className="absolute -top-2 -left-2 bg-secondary text-secondary-content rounded-full w-8 h-8 flex items-center justify-center font-bold text-sm z-10 shadow-lg">
+                                    {i + 1}
                                 </div>
                                 <B50Card info={song} />
                             </div>
