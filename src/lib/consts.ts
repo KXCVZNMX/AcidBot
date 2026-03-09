b5e601f617a00fb126af39ffb5c04d2a8d1b26d7
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-03-10 00:03:03 +1100

    fix: KoP Songs

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -366,4 +366,6 @@ export const CIRCLE_SONGS: Readonly<string[]> = [
     '恋の氷結おてんば湯けむりチルノ温泉',
     'イタ電はやめて！ ぼくらのスカーレット・コール',
     'むしみこうにゃーのハッピッピー',
+    '7 Wonders',
+    'Break The Speakers',
 ];
