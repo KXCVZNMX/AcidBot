f7e1b734f7279c505e22ca44803ead19a63f8b2f
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-05-06 10:27:40 +1000

    feat(pwa): included manifest.ts and icons

---
+++ src/app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'AcidBot',
+        short_name: 'AcidBot',
+        description: 'A tool to fetch your results for maimai でらっくす International Ver.',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#171212',
+        theme_color: '#171212',
+        icons: [
+            {
+                'src': '/web-app-manifest-192x192.png',
+                'sizes': '192x192',
+                'type': 'image/png',
+                'purpose': 'maskable'
+            },
+            {
+                'src': '/web-app-manifest-512x512.png',
+                'sizes': '512x512',
+                'type': 'image/png',
+                'purpose': 'maskable'
+            }
+        ],
+    }
+}
