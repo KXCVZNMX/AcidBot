a72e211c7ab4bc86247f0f6584da63c0cd727ce9
KXCVZNMX <CC6391@scotchmel.vic.edu.au>
2026-05-15 20:41:25 +1000

    feat(b50Image api): competed

--- src/app/api/v2/user/[id]/b50Image/route.tsx
+++ src/app/api/v2/user/[id]/b50Image/route.tsx
@@ -9,6 +9,7 @@ import {MSSB50, ParsedProfile} from '@/lib/types';
 import {ImageResponse} from 'next/og';
 import {readFileSync} from 'fs';
 import {join} from 'path';
+import React from 'react';
 
 export const alt = 'B50'
 export const runtime = 'nodejs';
@@ -41,6 +42,321 @@ function getFonts() {
 const chooseNP = (nps: string[]) => {
     const randomIndex = Math.floor(Math.random() * nps.length);
     return nps[randomIndex];
+}
+
+const determineBackgroundColor = (diff: string) => {
+    if (diff === 'remaster') {
+        return '#DFC7F8';
+    } else if (diff === 'master') {
+        return '#9e45e2';
+    } else if (diff === 'expert') {
+        return '#f64861';
+    } else if (diff === 'advanced') {
+        return '#fb9c2d';
+    } else if (diff === 'basic') {
+        return '#22bb5b';
+    } else {
+        return '#22bb5b';
+    }
+};
+
+const determineRankImage = (rank: string | null) => {
+    if (rank === 'SSS+') {
+        return '/b50/SSSp.png';
+    } else if (rank === 'SSS') {
+        return '/b50/SSS.png';
+    } else if (rank === 'SS+') {
+        return '/b50/SSp.png';
+    } else if (rank === 'SS') {
+        return '/b50/SS.png';
+    } else if (rank === 'S+') {
+        return '/b50/Sp.png';
+    } else if (rank === 'S') {
+        return '/b50/S.png';
+    } else if (rank === 'AAA') {
+        return '/b50/AAA.png';
+    } else if (rank === 'AA') {
+        return '/b50/AA.png';
+    } else if (rank === 'A') {
+        return '/b50/A.png';
+    } else if (rank === 'BBB') {
+        return '/b50/BBB.png';
+    } else if (rank === 'BB') {
+        return '/b50/BB.png';
+    } else if (rank === 'B') {
+        return '/b50/B.png';
+    } else if (rank === 'C') {
+        return '/b50/C.png';
+    } else {
+        return '/b50/D.png';
+    }
+};
+
+const determineComboImage = (combo: string | null) => {
+    if (combo === 'AP+') {
+        return '/b50/music_icon_app.png';
+    } else if (combo === 'AP') {
+        return '/b50/music_icon_ap.png';
+    } else if (combo === 'FC+') {
+        return '/b50/music_icon_fcp.png';
+    } else if (combo === 'FC') {
+        return '/b50/music_icon_fc.png';
+    } else {
+        return '/b50/music_icon_back.png';
+    }
+};
+
+const determineSyncImage = (sync: string | null) => {
+    if (sync === 'FDX+') {
+        return '/b50/music_icon_fdxp.png';
+    } else if (sync === 'FDX') {
+        return '/b50/music_icon_fdx.png';
+    } else if (sync === 'FS+') {
+        return '/b50/music_icon_fsp.png';
+    } else if (sync === 'FS') {
+        return '/b50/music_icon_fs.png';
+    } else if (sync === 'SYNC') {
+        return '/b50/music_icon_sync.png';
+    } else {
+        return '/b50/music_icon_back.png';
+    }
+};
+
+const determineStarCount = (dx: string) => {
+    const dxScore = dx.split('/');
+    const achievedDx = parseInt(dxScore[0].replace(/,/g, ''));
+    const maxDx = parseInt(dxScore[1].replace(/,/g, ''));
+    const percentage = achievedDx / maxDx;
+    const SITE_LINK = process.env.SITE_LINK ?? 'http://localhost:3000';
+    if (percentage >= 0.97) {
+        return (
+            <img
+                src={`${SITE_LINK}/b50/music_icon_dxstar_detail_5.png`}
+                alt={'dx stars'}
+                width={52}
+                style={{
+                    position: 'absolute',
+                    top: 86,
+                    left: 202
+                }}
+            />
+        );
+    } else if (percentage >= 0.95) {
+        return (
+            <img
+                src={`${SITE_LINK}/b50/music_icon_dxstar_detail_4.png`}
+                alt={'dx stars'}
+                width={55}
+                style={{
+                    position: 'absolute',
+                    top: 86,
+                    left: 201
+                }}
+            />
+        );
+    } else if (percentage >= 0.93) {
+        return (
+            <img
+                src={`${SITE_LINK}/b50/music_icon_dxstar_detail_3.png`}
+                alt={'dx stars'}
+                width={60}
+                style={{
+                    position: 'absolute',
+                    top: 85,
+                    left: 198
+                }}
+            />
+        );
+    } else if (percentage >= 0.9) {
+        return (
+            <img
+                src={`${SITE_LINK}/b50/music_icon_dxstar_detail_2.png`}
+                alt={'dx stars'}
+                width={65}
+                style={{
+                    position: 'absolute',
+                    top: 85,
+                    left: 196
+                }}
+            />
+        );
+    } else if (percentage >= 0.85) {
+        return (
+            <img
+                src={`${SITE_LINK}/b50/music_icon_dxstar_detail_1.png`}
+                alt={'dx stars'}
+                width={65}
+                style={{
+                    position: 'absolute',
+                    top: 85,
+                    left: 194
+                }}
+            />
+        );
+    } else {
+        return null;
+    }
+};
+
+function B50Card({ info }: { info: MSSB50 }) {
+    const SITE_LINK = process.env.SITE_LINK ?? 'http://localhost:3000';
+    const backgroundColor = determineBackgroundColor(info.diff);
+    const textColor = info.diff === 'remaster' ? '#9e45e2' : '#fff';
+    const textShadow =
+        info.diff === 'remaster'
+            ? `
+      1px 1px 1px rgba(130,130,130,0.6),
+      1px 2px 2px rgba(130,130,130,0.5)
+    `
+            : `
+      1px 1px 1px rgba(0,0,0,0.6),
+      1px 2px 2px rgba(0,0,0,0.5)
+    `;
+
+    return (
+        <div
+            style={{
+                backgroundColor: 'white',
+                width: 265,
+                height: 110,
+                borderRadius: 12,
+                paddingTop: 4,
+                display: 'flex',
+                flex: '0 0 265px',
+                flexShrink: 0,
+            }}
+        >
+            <div
+                style={{
+                    position: 'absolute',
+                    marginInline: 'auto',
+                    height: 75,
+                    width: 255,
+                    borderTopLeftRadius: 12,
+                    borderTopRightRadius: 12,
+                    left: 5,
+                    display: 'flex',
+                    backgroundColor,
+                    color: textColor,
+                }}
+            >
+                <img
+                    src={`${SITE_LINK}/maimaiJackets/${info.jacketURL}`}
+                    alt={'jacket'}
+                    width={75}
+                    height={75}
+                    style={{
+                        position: 'absolute',
+                        left: 12,
+                        top: 12,
+                        borderWidth: 4,
+                        borderBottomWidth: 0,
+                        borderColor: 'white',
+                        borderRadius: 8
+                    }}
+                />
+                <h2
+                    style={{
+                        position: 'absolute',
+                        left: 96,
+                        top: -18,
+                        fontFamily: 'mPlusMed',
+                        fontSize: 16,
+                        color: textColor
+                    }}
+                >
+                    {truncateByWidth(info.name, 20)}
+                </h2>
+                <hr
+                    style={{
+                        position: 'absolute',
+                        left: 87,      
+                        top: 15,
+                        width: 168,    
+                        height: 2,     
+                        backgroundColor: 'white',
+                        border: 'none',
+                    }}
+                />
+                <h1
+                    style={{
+                        position: 'absolute',
+                        left: 92,      
+                        top: 2,
+                        fontSize: 26,
+                        fontWeight: 500,
+                        color: textColor,
+                        textShadow,
+                    }}
+                >
+                    {`${info.achievement.toFixed(4)}%`}
+                </h1>
+                <p
+                    style={{
+                        position: 'absolute',
+                        left: 96,      
+                        top: 38,
+                        fontSize: 12, 
+                        color: textColor,
+                    }}
+                >
+                    {`${info.levelConst.toFixed(1)} → ${info.rating}`}
+                </p>
+                <p
+                    style={{
+                        position: 'absolute',
+                        left: 171,     
+                        top: 38,
+                        fontSize: 12,  
+                        color: textColor,
+                    }}
+                >
+                    {info.dx}
+                </p>
+                <img
+                    src={info.isDx === 'dx' ? `${SITE_LINK}/b50/music_dx.png` : `${SITE_LINK}/b50/music_standard.png`}
+                    alt={'music_dx_std'}
+                    width={50}
+                    style={{
+                        position: 'absolute',
+                        top: 88,
+                        left: 24
+                    }}
+                />
+                <img
+                    src={SITE_LINK + determineRankImage(info.rank)}
+                    alt={'rank'}
+                    width={50}
+                    style={{
+                        position: 'absolute',
+                        top: 79,
+                        left: 90
+                    }}
+                />
+                <img
+                    src={SITE_LINK + determineComboImage(info.combo)}
+                    alt={'combo'}
+                    width={28}
+                    style={{
+                        position: 'absolute',
+                        top: 75,
+                        left: 145
+                    }}
+                />
+                <img
+                    src={SITE_LINK + determineSyncImage(info.sync)}
+                    alt={'sync'}
+                    width={28}
+                    style={{
+                        position: 'absolute',
+                        top: 75,
+                        left: 173
+                    }}
+                />
+                {determineStarCount(info.dx)}
+            </div>
+        </div>
+    )
 }
 
 export async function GET(
@@ -207,7 +523,6 @@ export async function GET(
                         top: 50,
                         backgroundColor: '#6a7282',
                         borderRadius: 8,
-                        zIndex: 5
                     }}
                 />
                 <div
@@ -222,7 +537,6 @@ export async function GET(
                         borderColor: '#6a7282',
                         borderWidth: '2px',
                         borderStyle: 'solid',
-                        zIndex: 10,
                     }}
                 />
                 <img
@@ -234,7 +548,6 @@ export async function GET(
                         position: 'absolute',
                         top: 50,
                         left: 317,
-                        zIndex: 20
                     }}
                 />
                 <img
@@ -246,7 +559,6 @@ export async function GET(
                         position: 'absolute',
                         top: 53,
                         left: 440,
-                        zIndex: 20
                     }}
                 />
                 <div
@@ -262,7 +574,6 @@ export async function GET(
                         borderColor: '#99a1af',
                         borderWidth: 2,
                         borderRadius: 8,
-                        zIndex: 20
                     }}
                 />
                 <img
@@ -274,7 +585,6 @@ export async function GET(
                         position: 'absolute',
                         top: 79,
                         left: 570,
-                        zIndex: 20,
                     }}
                 />
                 <div
@@ -285,7 +595,6 @@ export async function GET(
                         left: 648,
                         color: '#ffffff',
                         letterSpacing: 3,
-                        zIndex: 20
                     }}
                 >
                     {rating}
@@ -299,7 +608,6 @@ export async function GET(
                         position: 'absolute',
                         top: 116,
                         left: 425,
-                        zIndex: 20
                     }}
                 />
                 <img
@@ -311,7 +619,6 @@ export async function GET(
                         position: 'absolute',
                         top: 114,
                         left: 505,
-                        zIndex: 20
                     }}
                 />
                 <img
@@ -323,7 +630,6 @@ export async function GET(
                         position: 'absolute',
                         top: 120,
                         left: 574,
-                        zIndex: 20
                     }}
                 />
                 <p
@@ -333,7 +639,6 @@ export async function GET(
                         left: 604,
                         color: '#101828',
                         fontWeight: 500,
-                        zIndex: 20
                     }}
                 >
                     {profile.userCollectionCount!.text!}
@@ -344,7 +649,6 @@ export async function GET(
                         top: 36,
                         left: 465,
                         fontSize: 14,
-                        zIndex: 20,
                     }}
                 >
                     {truncateByWidth(profile.userDetail!, 28)}
@@ -354,11 +658,66 @@ export async function GET(
                         paddingLeft: 4,
                         top: 68,
                         left: 427,
-                        zIndex: 20
                     }}
                 >
                     {truncateByWidth(profile.userName!, 12)}
                 </p>
+
+                <div
+                    style={{
+                        position: 'absolute',
+                        top: 230,
+                        left: 0,
+                        width: 1400,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        padding: 12,
+                        gap: 70,
+                    }}
+                >
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            gap: 8,
+                            width: '100%',
+                            alignContent: 'flex-start',
+                        }}
+                    >
+                        {b35.map((s, idx) => (
+                            <B50Card info={s} key={`old-${idx}`} />
+                        ))}
+                    </div>
+
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            gap: 8,
+                            width: '100%',
+                            alignContent: 'flex-start',
+                        }}
+                    >
+                        {b15.map((s, idx) => (
+                            <B50Card info={s} key={`new-${idx}`} />
+                        ))}
+                    </div>
+                </div>
+                <h3
+                    style={{
+                        position: 'absolute',
+                        bottom: 12,
+                        left: 0,
+                        right: 0,
+                        display: 'flex',
+                        justifyContent: 'center',
+                        color: 'white',
+                        fontFamily: 'MPlusMed',
+                        fontSize: 20,
+                    }}
+                >
+                    Designed by KVZ. Generated by AcidBot
+                </h3>
             </div>
         ),
         {
