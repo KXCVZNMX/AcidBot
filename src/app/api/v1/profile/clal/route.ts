330ee45abc1c0ff66c230c433a6975a0a1175fb3
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-06-11 17:56:59 +1000

    chores(clal): rollback, keep using GET. ugh fuck REST

--- src/app/api/v1/profile/clal/route.ts
+++ src/app/api/v1/profile/clal/route.ts
@@ -2,20 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import client from '@/lib/db';
 import { ObjectId } from 'mongodb';
 
-function corsResponse(response: NextResponse) {
-    // You can change '*' to 'https://lng-tgk-aime-gw.am-all.net' for strict security
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Allow-Methods', 'PUT, OPTIONS');
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    return response;
-}
-
-export async function OPTIONS() {
-    const response = new NextResponse(null, { status: 204 });
-    return corsResponse(response);
-}
-
-export async function PUT(req: NextRequest) {
+export async function GET(req: NextRequest) {
     const url = req.nextUrl;
 
     try {
@@ -47,12 +34,10 @@ export async function PUT(req: NextRequest) {
             maxAge: 60 * 60 * 24 * 365,
         });
 
-        return corsResponse(ret);
+        return ret;
     } catch {
-        const errRet = NextResponse.redirect(new URL('/pages/ClalFetchFailure', url), {
+        return NextResponse.redirect(new URL('/pages/ClalFetchFailure', url), {
             status: 302,
         });
-
-        return corsResponse(errRet);
     }
 }
