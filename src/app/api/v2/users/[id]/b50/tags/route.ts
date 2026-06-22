5d6218be113c811104e503edfeba3fab4d4a4a56
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-06-23 00:42:19 +1000

    fix(tags api): Next.js requires NextRequest in the api signatures

--- src/app/api/v2/users/[id]/b50/tags/route.ts
+++ src/app/api/v2/users/[id]/b50/tags/route.ts
@@ -1,5 +1,5 @@
 import {z} from 'zod';
-import {NextResponse} from 'next/server';
+import {NextRequest, NextResponse} from 'next/server';
 import {DatabaseError, MalformedRequest, UserNotFoundOrNoPrev} from '@/app/api/v2/_shared/types';
 import client from '@/lib/db';
 import {ObjectId} from 'mongodb';
@@ -10,6 +10,7 @@ export const TagsSchema = z.object({
 });
 
 export async function GET(
+    req: NextRequest,
     { params }: { params: Promise<{ id: string }> }
 ) {
     const { id: u_id } = await params;
