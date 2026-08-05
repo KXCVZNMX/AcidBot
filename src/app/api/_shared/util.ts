be39a3230b6d857c2ef8adb73e0a7fef88d96b83
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-08-05 23:56:54 +1000

    feat: addded temperory update information copied from jp datas

--- src/app/api/_shared/util.ts
+++ src/app/api/_shared/util.ts
@@ -8,6 +8,7 @@ import {
 import { SongInfo, SongInfoProjection } from '@/app/api/_shared/types';
 import * as cheerio from 'cheerio';
 import { Db } from 'mongodb';
+import songs from '../_shared/temp_updates.json'
 
 export const songInfoProjection: SongInfoProjection = {
     title: 1,
@@ -118,9 +119,15 @@ export async function getSongInfoMap(
         )
         .toArray();
 
+    const tempUpdatedSongs = songs as unknown as SongInfo[];
+
     const docMap = new Map<string, SongInfo>();
     for (const d of docs) {
         if (d?.title) docMap.set(d.title, d);
+    }
+
+    for (const s of tempUpdatedSongs) {
+        docMap.set(s.title, s);
     }
 
     return docMap;
